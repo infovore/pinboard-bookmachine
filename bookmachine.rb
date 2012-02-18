efc1243fe5d7aca76c7928d2c9323f3ff2cab25a
Tom Armitage <tom@infovore.org>
2012-02-18 10:43:51 +0000

    Working year teampltes.

--- bookmachine.rb
+++ bookmachine.rb
@@ -16,14 +16,61 @@ get '/stylesheets/:stylesheet.css' do
 end
 
 get '/year/:year' do
-  @year = params[:year]
+  @year = params[:year].to_i
   @bookmarks = Bookmark.order("bookmarked_at").where("bookmarked_at > ?", DateTime.new(@year,1,1,0,0,0)).where("bookmarked_at < ?", DateTime.new(@year + 1,1,1,0,0,0))
   @bookmarks_by_month = @bookmarks.group_by(&:month)
+
+  @title = "A Year of Links: #{@year}"
   haml :year, :layout => :print
+end
+
+# helpers
+
+helpers do
+  def link_to(*args, &block)
+  end
+
+  def format_date(date)
+    date.strftime("%d %B %Y")
+  end
+
+  def mark_hash(string)
+    Digest::MD5.hexdigest(string)
+  end
+
+  def id_for_tag(tag_string, previous_tags_array)
+    if previous_tags_array.include?(tag_string)
+      previous_tags_array << tag_string
+      count = previous_tags_array.count(tag_string)
+      ["#{tag_string}-#{count}",previous_tags_array]
+    else
+      previous_tags_array << tag_string
+      ["#{tag_string}-1", previous_tags_array]
+    end
+  end
+
+  def volume_number_for_year(year)
+    year.to_i - 2004 + 1
+  end  
 end
 
 # models
 
 class Bookmark < ActiveRecord::Base
+  def tags
+    raw_tags.split(" ")
+  end
 
+  def month
+    bookmarked_at.strftime('%B')
+  end
+
+  def day
+    bookmarked_at.strftime("%d")
+  end
+
+  def qr_for_url
+    "http://qrcode.kaywa.com/img.php?s=8&d=#{CGI.escape(href)}"
+  end
+  
 end
